31860e7a902b4a8fd7c0a4f5f46f9d7e25a442b2
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-07 17:58:54 +0530

    home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,8 @@
 function Home() {
   return (
-    <div className="w-full min-h-screen h-full pt-10 gap-5 flex flex-col items-center bg-gray-200"></div>
+    <div className="w-full min-h-screen h-full pt-16 gap-5 flex flex-col items-center bg-gray-200">
+      adsadasd
+    </div>
   );
 }
 
