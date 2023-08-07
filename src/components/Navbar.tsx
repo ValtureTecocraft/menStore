90af55630ff37866cc378e3862b8d0ff683f4cf2
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-07 14:11:45 +0530

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import { AiFillHeart, AiOutlineClose } from "react-icons/ai";
+import { MdShoppingCart } from "react-icons/md";
+import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+
+interface IState {
+  toggle: boolean;
+  search: string;
+}
+
+const Navbar: React.FC = () => {
+  const [state, setState] = useState<IState>({
+    toggle: false,
+    search: "",
+  });
+
+  //   const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    setState({
+      ...state,
+      toggle: !state.toggle,
+      search: "",
+    });
+  };
+
+  return (
+    <div className="w-full flex justify-center shadow-lg items-center">
+      <div className="relative max-w-7xl w-full py-3 px-5 flex justify-between items-center">
+        <div
+          className={`w-fit transition-transform duration-300 flex justify-center items-center border-2 rounded ${
+            state.toggle ? "border-gray-600" : "border-transparent"
+          }`}
+        >
+          <input
+            className={`duration-300 transition-[width] outline-none ${
+              state.toggle ? "w-40 px-2" : "w-0"
+            }`}
+            placeholder="Search ..."
+            type="text"
+            name="search"
+            value={state.search}
+            onChange={(e) => setState({ ...state, search: e.target.value })}
+          />
+          <button onClick={handleSearchClick} type="button">
+            {state.toggle ? (
+              <AiOutlineClose
+                // onClick={() => setState({ ...state, search: "" })}
+                className="text-xl text-gray-700"
+              />
+            ) : (
+              <BiSearchAlt className="text-2xl text-gray-500" />
+            )}
+          </button>
+        </div>
+
+        <Link
+          to={"/"}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer text-3xl font-bold select-none"
+        >
+          Men's Shop
+        </Link>
+
+        <div className="gap-4 text-2xl flex justify-center items-center">
+          <button
+            className=" w-24 h-10 duration-300 ease-in transition-all flex justify-center items-center pb-1 rounded hover:rounded-full hover:bg-gray-400 bg-gray-300"
+            type="button"
+          >
+            Login
+          </button>
+          <AiFillHeart />
+          <div>
+            <MdShoppingCart />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,6 @@
 function Home() {
   return (
-    <div className="w-full min-h-screen h-full pt-10 gap-5 flex flex-col items-center">
-      <h1 className="font-bold text-lg text-blue-700">Person List App</h1>
-    </div>
+    <div className="w-full min-h-screen h-full pt-10 gap-5 flex flex-col items-center bg-gray-200"></div>
   );
 }
 
