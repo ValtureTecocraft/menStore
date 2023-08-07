fe898b5882cb3cd314caee14696001558d0e05e3
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-07 11:06:51 +0530

    init

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,7 @@
-import Form from "../components/Form";
-import List from "../components/List";
-
 function Home() {
   return (
     <div className="w-full min-h-screen h-full pt-10 gap-5 flex flex-col items-center">
       <h1 className="font-bold text-lg text-blue-700">Person List App</h1>
-      <Form />
-      <List />
     </div>
   );
 }
