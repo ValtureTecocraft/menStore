bbb73406d530e80e104b9fb129ca4274fcec4d6b
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-14 12:24:56 +0530

    init

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
-function Home() {
+import React from "react";
+
+const Home: React.FC = () => {
   return (
     <div className="w-full min-h-screen h-full pt-16 gap-5 flex flex-col items-center bg-gray-200">
       adsadasd
     </div>
   );
-}
+};
 
 export default Home;
