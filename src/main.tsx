f906528af6979735480326846ae5faf62bb6a068
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-14 12:23:11 +0530

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/store.js";
-import { AuthProvider } from "./context/auth.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,7 @@
-import Counter from "../components/Counter";
-import CounterButton from "../components/CounterButton";
-import DocumentTitle from "../components/DocumentTitle";
-
 function Home() {
   return (
     <div className="w-full min-h-screen h-full pt-16 gap-5 flex flex-col items-center bg-gray-200">
       adsadasd
-      {/* <Counter /> */}
-      <DocumentTitle />
-      <CounterButton />
     </div>
   );
 }
