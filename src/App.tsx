df671990a1ce2e1a1d1e1700bdcb2349efe5d4cd
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-22 16:43:18 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import NavBar from "./components/NavBar";
-import { Crud_api } from "./screens/Crud_api";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/crud-api" element={<Crud_api />} />
       </Routes>
     </>
   );
