c6d46ffb2fef648fb2e6f6ff109de91f1fb090b9
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-23 13:01:08 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
-import Navbar from "./components/Navbar";
-import Login from "./screens/Login";
-import Signup from "./screens/Signup";
-import Products from "./screens/Products";
-import { RequireAuth } from "./components/RequireAuth";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route
-          path="/products"
-          element={
-            <RequireAuth>
-              <Products />
-            </RequireAuth>
-          }
-        />
       </Routes>
     </>
   );
--- src/screens/Products.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Products: React.FC = () => {
-  return <div className="pt-32">Products</div>;
-};
-
-export default Products;
