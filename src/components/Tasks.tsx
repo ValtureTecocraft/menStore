cd1fcf42e2983b414547287f5ba814688e7d9798
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-23 18:33:39 +0530

    home page

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -13,22 +13,18 @@ const Tasks = ({
   const [toggle, setToggle] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
-  //   useEffect(() => {
-  //     const storedToggle = localStorage.getItem(`toggle_${
-  //         // item
-  //     }`);
-  //     if (storedToggle) {
-  //       setToggle(JSON.parse(storedToggle));
-  //     }
-  //   }, []);
+  useEffect(() => {
+    const storedToggle = localStorage.getItem(`toggle_${item}`);
+    if (storedToggle) {
+      setToggle(JSON.parse(storedToggle));
+    }
+  }, []);
 
-  //   const handleToggle = () => {
-  //     const updatedToggle = !toggle;
-  //     setToggle(updatedToggle);
-  //     localStorage.setItem(`toggle_${
-  //         // item
-  //     }`, JSON.stringify(updatedToggle));
-  //   };
+  const handleToggle = () => {
+    const updatedToggle = !toggle;
+    setToggle(updatedToggle);
+    localStorage.setItem(`toggle_${item}`, JSON.stringify(updatedToggle));
+  };
 
   //   const handleDelete = () => {
   //     // onDelete(
@@ -41,7 +37,7 @@ const Tasks = ({
 
   return (
     <div
-      className={`p-5 text-xl cursor-move border-b flex gap-3 justify-between items-center ${
+      className={`p-5 text-xl cursor-move border-b flex gap-3 justify-start items-center ${
         isDarkMode
           ? "text-gray-300 border-gray-700"
           : "text-gray-700 border-gray-300"
@@ -50,8 +46,8 @@ const Tasks = ({
       onMouseLeave={() => setIsHovered(false)}
     >
       <div
-        // onClick={handleToggle}
-        className={`md:w-9 w-8 h-[26px] md:h-8 cursor-pointer flex justify-center items-center rounded-full ${
+        onClick={handleToggle}
+        className={`w-8 h-[26px] md:h-8 cursor-pointer flex justify-center items-center rounded-full ${
           toggle && "bg-gradient-to-br"
         } from-[#7bbbf9] to-[#8064c6] border-2 ${
           isDarkMode ? "border-[#353648]" : "border-gray-300"
@@ -60,7 +56,7 @@ const Tasks = ({
         {toggle && <img src={icon_check} alt="icon check" />}
       </div>
       <p
-        className={`ml-2 pt-2 w-full ${toggle && "line-through text-gray-500"}`}
+        className={`ml-2 pt-2 w-5/6 ${toggle && "line-through text-gray-500"}`}
       >
         {item}
       </p>
