f44c1890d40292d4bc4ad962cdb60fec9d725abf
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-24 15:54:12 +0530

    page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import { SignIn } from "./screens/SignIn";
+import { PageNotFound } from "./screens/PageNotFound";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="*" element={<PageNotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
