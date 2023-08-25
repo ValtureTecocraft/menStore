70aa079d8b091f0458f5f86dc74179cfa38d7e4b
ValtureTecocraft <smit.valture@tecocraft.com>
2023-08-25 09:36:25 +0530

    login page img

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import loginBg from "../assets/login-bg.jpg";
+import loginBg from "../assets/login-bg.webp";
 import { Link, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
